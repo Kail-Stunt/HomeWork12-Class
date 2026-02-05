13fd7c65cfe4a2ad5f71e88b678e5f07e4ea69d0
Kail-Stunt <Kail-Stunt@mail.ru>
2026-02-05 18:35:55 +0700

    Исправленное домашнее задание

--- src/Book.java
+++ src/Book.java
@@ -1,22 +1,27 @@
-public class Book {//Создаём новый класс Book с информацией о названии и даие публикации книги
+public class Book {//Создаём новый класс Book с информацией о названии и дате публикации книги
     //Объявляем поля класса и защищаем их через private
-    private String bookTitle;
-    //private Author author; А зачем мы вводим это, поле если далее создаём класс и вызываем его из main?
+    private final String bookTitle;
+    private final Author author;
     private int yearOfPublication;
+
     //Создаём конструктор для полей класса
-    public Book(String bookTitle, int yearOfPublication) {
+    public Book(String bookTitle, int yearOfPublication, Author author) {
         this.bookTitle = bookTitle;
         this.yearOfPublication = yearOfPublication;
+        this.author = author;
+        //author = author.getFirstName()+author.getLastName();
     }
 
     //Создаём get для названия книги
     public String getBookTitle() {
         return this.bookTitle;
     }
+
     //Создаём get для даты публикации книги
     public int getYearOfPublication() {
         return this.yearOfPublication;
     }
+
     //Создаём set для даты публикации книги
     public void setYearOfPublication(int yearOfPublication) {
         this.yearOfPublication = yearOfPublication;
--- src/Main.java
+++ src/Main.java
@@ -2,11 +2,11 @@ public class Main {
     public static void main(String[] args) {
         System.out.println("Вывод в консоль 1:\nВывести данные о книге, дате издания и авторе:\n");
         Author stihpenKing = new Author("Стивен", "Кинг");
-        Book theMist = new Book("Мгла", 1980);
+        Book theMist = new Book("Мгла", 1980, stihpenKing);
         System.out.println("Книга = \"" + theMist.getBookTitle() + "\" издана в " + theMist.getYearOfPublication());
         System.out.println("Автор = " + stihpenKing.getFirstName() + " " + stihpenKing.getLastName());
         Author richardMatheson = new Author("Ричард", "Мэтисон");
-        Book iAmLegend = new Book("Я легенда", 1952);
+        Book iAmLegend = new Book("Я легенда", 1952, richardMatheson);
         System.out.println("Книга = \"" + iAmLegend.getBookTitle() + "\" издана в " + iAmLegend.getYearOfPublication());
         System.out.println("Автор = " + richardMatheson.getFirstName() + " " + richardMatheson.getLastName());
         System.out.println("\nВывод в консоль 2:\nСкорректировать дату издания:\n");
